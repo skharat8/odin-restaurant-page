5d45dd61a55ff08a5f19f6ec7c127dd9ae8ca024
enigma9119 <enigma9119@users.noreply.github.com>
2023-12-03 20:40:20 -0800

    Add basic styles & a nav bar on page load

---
+++ src/home_page.js
@@ -0,0 +1,5 @@
+const contentDiv = document.querySelector("div#content");
+
+export default function loadHomePage(e) {
+  console.log("HOME");
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import "normalize.css";
+import "./styles.css";
 
-console.log("Testing...");
+import loadHomePage from "./home_page.js";
+
+const contentDiv = document.querySelector("div#content");
+
+function initialPageLoad() {
+  const nav = document.createElement("nav");
+  const ul = document.createElement("ul");
+  const homePage = document.createElement("li");
+  const menuPage = document.createElement("li");
+  const aboutPage = document.createElement("li");
+
+  const homePageLink = document.createElement("a");
+  const menuPageLink = document.createElement("a");
+  const aboutPageLink = document.createElement("a");
+
+  const homePageText = document.createTextNode("Home Page");
+  const menuPageText = document.createTextNode("Menu Page");
+  const aboutPageText = document.createTextNode("About Page");
+
+  nav.appendChild(ul);
+  ul.append(homePage, menuPage, aboutPage);
+  homePage.appendChild(homePageLink);
+  menuPage.appendChild(menuPageLink);
+  aboutPage.appendChild(aboutPageLink);
+
+  homePageLink.appendChild(homePageText);
+  menuPageLink.appendChild(menuPageText);
+  aboutPageLink.appendChild(aboutPageText);
+
+  homePageLink.setAttribute("id", "home-page");
+  menuPageLink.setAttribute("id", "menu-page");
+  aboutPageLink.setAttribute("id", "about-page");
+
+  contentDiv.appendChild(nav);
+}
+
+initialPageLoad();
+
+const homePage = document.querySelector("#home-page");
+const menuPage = document.querySelector("#menu-page");
+const aboutPage = document.querySelector("#about-page");
+
+homePage.addEventListener("click", loadHomePage);
